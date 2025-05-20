23c8c03d090640f5d2bde25711ab374799ca3bbc
Abraham R. Flores <arflores32@gmail.com>
2025-05-20 10:51:56 -0500

    initial commit

---
+++ init.f90
@@ -0,0 +1,133 @@
+! ----------------------------------------------------------------------
+! Abraham R. Flores
+! WASHINGTON UNIVERSITY IN ST. LOUIS
+! ALCF HACKATHON 2025
+! ----------------------------------------------------------------------
+MODULE RNG_M
+! ----------------------------------------------------------------------
+  use, intrinsic :: iso_fortran_env, only: spi=>int32, dpf=>real64,stdin=>input_unit
+! ----------------------------------------------------------------------
+  IMPLICIT NONE
+! ----------------------------------------------------------------------
+  CONTAINS
+! ----------------------------------------------------------------------
+    SUBROUTINE RANDINT(RNG_STATE,NMAX,N)
+      REAL(dpf),    INTENT(INOUT)  :: RNG_STATE
+      INTEGER(spi), INTENT(IN)     :: NMAX
+      INTEGER(spi), INTENT(OUT)    :: N
+      REAL(dpf), PARAMETER :: M1 = 2147483647._dpf
+      REAL(dpf), PARAMETER :: M = 2147483648._dpf
+      REAL(dpf), PARAMETER :: A = 48271._dpf
+      REAL(dpf) :: RNG
+      RNG_STATE=MOD(A*RNG_STATE,M1)
+      RNG=RNG_STATE/M*NMAX
+      N=INT(RNG,KIND=spi)+1 ! randint FROM 1 -> Nmax
+    END SUBROUTINE RANDINT
+! ----------------------------------------------------------------------
+END MODULE RNG_M
+! ----------------------------------------------------------------------
+MODULE NQMCC_M
+! ----------------------------------------------------------------------
+  use, intrinsic :: iso_fortran_env, only: spi=>int32,dpi=>int64, dpf=>real64
+! ----------------------------------------------------------------------
+  USE MPI_F08
+  USE RNG_M
+! ----------------------------------------------------------------------
+  IMPLICIT NONE
+! ----------------------------------------------------------------------
+  TYPE :: CONTROL_T
+    INTEGER(dpi) :: NPHIM
+    INTEGER(spi) :: NS,NT,NYLM,MAX_NZ
+    REAL(dpf) :: SEED
+    CONTAINS
+      PROCEDURE :: READ_CONTROL
+      PROCEDURE :: BROADCAST
+      PROCEDURE :: CONTROL_MAP_TO_DEVICE
+  END TYPE CONTROL_T
+! ----------------------------------------------------------------------
+  TYPE :: PHI_T
+    INTEGER(spi),DIMENSION(:),  ALLOCATABLE :: YLM_IDX
+    REAL(dpf),   DIMENSION(:),  ALLOCATABLE :: PHI_DAT
+    INTEGER(spi),DIMENSION(:,:),ALLOCATABLE :: NUM_ELEMENTS
+    INTEGER(dpi),DIMENSION(:,:),ALLOCATABLE :: START_IDX
+    CONTAINS
+      PROCEDURE :: ALLOCATE_PHI
+      PROCEDURE :: INIT_PHI
+      PROCEDURE :: PHI_MAP_TO_DEVICE
+  END TYPE PHI_T
+! ----------------------------------------------------------------------
+  CONTAINS
+! ----------------------------------------------------------------------
+  SUBROUTINE  READ_CONTROL(SELF)
+    CLASS(CONTROL_T),  INTENT(INOUT) :: SELF
+    OPEN(UNIT=STDIN,STATUS='OLD',ACTION='READ',FORM='FORMATTED')
+    READ(STDIN,*) SELF%NS
+    READ(STDIN,*) SELF%NT
+    READ(STDIN,*) SELF%NYLM
+    READ(STDIN,*) SELF%NPHIM
+    READ(STDIN,*) SELF%MAX_NZ
+    READ(STDIN,*) SELF%SEED
+    CLOSE(UNIT=STDIN)
+  END SUBROUTINE READ_CONTROL
+! ----------------------------------------------------------------------
+  SUBROUTINE  BROADCAST(SELF,ROOT,COMM,IERROR)
+    CLASS(CONTROL_T),INTENT(INOUT) :: SELF
+    TYPE(MPI_COMM),  INTENT(IN)    :: COMM
+    INTEGER(spi),    INTENT(IN)    :: ROOT
+    INTEGER(spi),    INTENT(INOUT) :: IERROR
+    CALL MPI_BCAST(SELF%NS,1,MPI_INTEGER4,ROOT,COMM,IERROR)
+    CALL MPI_BCAST(SELF%NT,1,MPI_INTEGER4,ROOT,COMM,IERROR)
+    CALL MPI_BCAST(SELF%NYLM,1,MPI_INTEGER4,ROOT,COMM,IERROR)
+    CALL MPI_BCAST(SELF%NPHIM,1,MPI_INTEGER8,ROOT,COMM,IERROR)
+    CALL MPI_BCAST(SELF%MAX_NZ,1,MPI_INTEGER4,ROOT,COMM,IERROR)
+    CALL MPI_BCAST(SELF%SEED,1,MPI_REAL8,ROOT,COMM,IERROR)
+  END SUBROUTINE BROADCAST
+! ----------------------------------------------------------------------
+  SUBROUTINE  CONTROL_MAP_TO_DEVICE(SELF)
+    CLASS(CONTROL_T),INTENT(INOUT) :: SELF
+    !$omp target enter data map(to: SELF)
+  END SUBROUTINE CONTROL_MAP_TO_DEVICE
+! ----------------------------------------------------------------------
+  SUBROUTINE  ALLOCATE_PHI(SELF,PARAMS)
+    CLASS(PHI_T),   INTENT(INOUT) :: SELF
+    TYPE(CONTROL_T),INTENT(IN)    :: PARAMS
+! ----------------------------------------------------------------------
+    ALLOCATE(SELF%YLM_IDX(PARAMS%NPHIM),SOURCE=0_spi)
+    ALLOCATE(SELF%PHI_DAT(PARAMS%NPHIM),SOURCE=0._dpf)
+    ALLOCATE(SELF%NUM_ELEMENTS(PARAMS%NS,PARAMS%NT),SOURCE=0_spi)
+    ALLOCATE(SELF%START_IDX(PARAMS%NS,PARAMS%NT),SOURCE=0_dpi)
+! ----------------------------------------------------------------------
+  END SUBROUTINE ALLOCATE_PHI
+! ----------------------------------------------------------------------
+  SUBROUTINE  INIT_PHI(SELF,PARAMS,STATE)
+    CLASS(PHI_T),   INTENT(INOUT) :: SELF
+    TYPE(CONTROL_T),INTENT(IN)    :: PARAMS
+    REAL(dpf),      INTENT(INOUT) :: STATE
+    INTEGER(spi) :: I,J,NX,NY
+    INTEGER(dpi) :: II
+! ----------------------------------------------------------------------
+    DO II=1,PARAMS%NPHIM
+      CALL RANDINT(STATE,PARAMS%NYLM,NY)
+      SELF%YLM_IDX(II)=NY
+      SELF%PHI_DAT(II)=REAL(NY,KIND=dpf)/PARAMS%NYLM
+    END DO
+    II=1
+    DO J=1,PARAMS%NT
+      DO I=1,PARAMS%NT
+        CALL RANDINT(STATE,PARAMS%MAX_NZ,NX)
+        SELF%NUM_ELEMENTS(I,J)=NX
+        SELF%START_IDX(I,J)=II
+        II=II+NX
+      END DO
+    END DO
+    PRINT *, "HOW FULL",REAL(II,kind=dpf)/PARAMS%NPHIM
+! ----------------------------------------------------------------------
+  END SUBROUTINE INIT_PHI
+! ----------------------------------------------------------------------
+  SUBROUTINE  PHI_MAP_TO_DEVICE(SELF)
+    CLASS(PHI_T),INTENT(INOUT) :: SELF
+    !!$omp target enter data map(to: SELF,SELF%K_START(:,:),SELF%K_END(:,:))
+  END SUBROUTINE PHI_MAP_TO_DEVICE
+! ----------------------------------------------------------------------
+END MODULE NQMCC_M
+! ----------------------------------------------------------------------
---
+++ nqmcc.f90
@@ -0,0 +1,99 @@
+PROGRAM NQMCC_ALCF_2025
+! ----------------------------------------------------------------------
+  use, intrinsic :: iso_fortran_env, only: spi=>int32, dpi=>int64, dpf=>real64
+! ----------------------------------------------------------------------
+  USE NQMCC_M
+  USE MPI_F08
+  USE OMP_LIB
+! ----------------------------------------------------------------------
+  IMPLICIT NONE
+! ----------------------------------------------------------------------
+  TYPE(MPI_COMM)  :: COMM
+  TYPE(CONTROL_T) :: PARAMS
+  TYPE(PHI_T)     :: PHI
+  REAL(dpf)       :: STATE,FIRST,INIT,LAST
+  COMPLEX(dpf)    :: CX
+  INTEGER(spi)    :: I,J,RANK,SIZE,IERROR,ROOT
+  INTEGER(dpi)    :: N,FIRST_IDX,LAST_IDX
+  COMPLEX(dpf), DIMENSION(:,:), ALLOCATABLE :: PSIJ
+  COMPLEX(dpf), DIMENSION(:),   ALLOCATABLE :: YLM_PROD
+! ----------------------------------------------------------------------
+  CALL MPI_INIT(IERROR)
+  CALL MPI_COMM_RANK(COMM, RANK, IERROR)
+  CALL MPI_COMM_SIZE(COMM, SIZE, IERROR)
+  ROOT=0_spi
+!-----------------------------------------------------------------------
+  FIRST=0._dpf
+  INIT=0._dpf
+  LAST=0._dpf
+!-----------------------------------------------------------------------
+  IF (RANK.EQ.ROOT) THEN
+    FIRST = omp_get_wtime()
+    CALL PARAMS%READ_CONTROL()
+  END IF
+  CALL PARAMS%BROADCAST(ROOT,COMM,IERROR)
+  CALL PHI%ALLOCATE_PHI(PARAMS)
+! ----------------------------------------------------------------------
+  ALLOCATE(PSIJ(PARAMS%NS,PARAMS%NT),SOURCE=CMPLX(0._dpf,0._dpf,KIND=dpf))
+  ALLOCATE(YLM_PROD(PARAMS%NYLM),SOURCE=CMPLX(1.12345_dpf,0.6789_dpf,KIND=dpf))
+! ----------------------------------------------------------------------
+  CALL PHI%INIT_PHI(PARAMS,STATE)
+  IF (RANK.EQ.ROOT) THEN
+    INIT = omp_get_wtime()
+    PRINT *, "INIT TIME (s)",INIT-FIRST
+  END IF
+! ----------------------------------------------------------------------
+  !MAX_IJ=IMAX*JMAX
+  !EXTRA = MOD(MAX_IJ,SIZE)
+  !PARTITION=MAX_IJ/SIZE
+  !IF (EXTRA.EQ.0) THEN
+  !  ISTART=1_spi+RANK*PARTITION
+  !  IEND=(1_spi+RANK)*PARTITION
+  !ELSE
+  !  IF (RANK.LT.EXTRA) THEN
+  !    ISTART = 1_spi+RANK*(PARTITION+1)
+  !    IEND = (1_spi+RANK)*(PARTITION+1)
+  !  ELSE 
+  !    ISTART = 1_spi+RANK*PARTITION+EXTRA
+  !    IEND = (1_spi+RANK)*PARTITION+EXTRA
+  !  END IF
+  !END IF
+! ----------------------------------------------------------------------
+  !TOTAL_SUM=0._dpf
+  !!$omp target teams distribute parallel do &
+  !!$omp& map(tofrom:TOTAL_SUM) &
+  !!$omp& map(to:K_DATA) &
+  !!$omp& private(k) &
+  !!$omp& shared(IMAX,JMAX,KMAX,K_DATA) &
+  !!$omp& reduction(+:total_sum)
+  !DO IDX=ISTART,IEND
+  !  I=1_spi+(IDX-1_spi)/JMAX
+  !  J=MOD((IDX-1_spi),JMAX)+1_spi
+  !  LOCAL_SUM=0._dpf
+  !  DO K=K_DATA%K_START(I,J),K_DATA%K_END(I,J)
+  !    LOCAL_SUM=LOCAL_SUM+GET_VALUE(K_DATA,I,J,K)
+  !  END DO
+  !  TOTAL_SUM=TOTAL_SUM+LOCAL_SUM
+  !END DO
+! ----------------------------------------------------------------------
+  DO J=1,PARAMS%NT
+    DO I=1,PARAMS%NS
+      CX=0._dpf
+      FIRST_IDX=PHI%START_IDX(I,J)
+      LAST_IDX=PHI%START_IDX(I,J)+PHI%NUM_ELEMENTS(I,J)-1
+      DO N=FIRST_IDX,LAST_IDX
+        CX=CX+YLM_PROD(PHI%YLM_IDX(N))*PHI%PHI_DAT(N)
+      END DO
+      PSIJ(I,J)=CX
+    END DO
+  END DO
+! ----------------------------------------------------------------------
+  IF (RANK.EQ.ROOT) THEN
+    LAST = omp_get_wtime()
+    PRINT *, "PHI TIME (s)",LAST-INIT
+    PRINT *, "TOTAL TIME (s)",LAST-FIRST
+  END IF
+! ----------------------------------------------------------------------
+  CALL MPI_FINALIZE(IERROR)
+! ----------------------------------------------------------------------
+END PROGRAM NQMCC_ALCF_2025
