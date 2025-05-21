3834bff2189212411e2e3cf7a07cd075cc6be872
Abraham R. Flores <arflores32@gmail.com>
2025-05-20 20:58:28 -0500

    add sample loop, greatly simplify phi loop

--- nqmcc.f90
+++ nqmcc.f90
@@ -13,11 +13,12 @@ PROGRAM NQMCC_ALCF_2025
   TYPE(PHI_T)     :: PHI
   REAL(dpf)       :: STATE,FIRST,INIT,LAST,DOT
   COMPLEX(dpf)    :: CX
-  INTEGER(spi)    :: I,J,RANK,SIZE,IERROR,ROOT,MAX_IJ,PARTITION,EXTRA,ISTART,IEND,IDX
-  INTEGER(dpi)    :: N,FIRST_IDX,LAST_IDX,IJ
+  INTEGER(spi)    :: I,J,RANK,SIZE,IERROR,ROOT,MAX_IJ
+  INTEGER(spi)    :: MY_SAMPLES,PARTITION,EXTRA,ISTART,IEND,IDX
+  INTEGER(dpi)    :: S,N
   INTEGER(dpi),DIMENSION(:), ALLOCATABLE :: MY_FIRST,MY_LAST
-  COMPLEX(dpf), DIMENSION(:,:), ALLOCATABLE :: PSIJ,PSIJ2
-  COMPLEX(dpf), DIMENSION(:),   ALLOCATABLE :: YLM_PROD
+  COMPLEX(dpf), DIMENSION(:,:), ALLOCATABLE :: PSIJ
+  COMPLEX(dpf), DIMENSION(:),   ALLOCATABLE :: YLM_PROD,PSIJ_FLAT
 ! ----------------------------------------------------------------------
   CALL MPI_INIT(IERROR)
   COMM=MPI_COMM_WORLD
@@ -28,6 +29,7 @@ PROGRAM NQMCC_ALCF_2025
   FIRST=0._dpf
   INIT=0._dpf
   LAST=0._dpf
+  DOT=0._dpf
 !-----------------------------------------------------------------------
   IF (RANK.EQ.ROOT) THEN
     FIRST = omp_get_wtime()
@@ -36,7 +38,6 @@ PROGRAM NQMCC_ALCF_2025
   CALL PARAMS%BROADCAST(ROOT,COMM,IERROR)
   CALL PHI%ALLOCATE_PHI(PARAMS)
 ! ----------------------------------------------------------------------
-  ALLOCATE(PSIJ2(PARAMS%NS,PARAMS%NT),SOURCE=CMPLX(0._dpf,0._dpf,KIND=dpf))
   ALLOCATE(PSIJ(PARAMS%NS,PARAMS%NT),SOURCE=CMPLX(0._dpf,0._dpf,KIND=dpf))
   ALLOCATE(YLM_PROD(PARAMS%NYLM),SOURCE=CMPLX(1.12345_dpf,0.6789_dpf,KIND=dpf))
 ! ----------------------------------------------------------------------
@@ -64,12 +65,15 @@ PROGRAM NQMCC_ALCF_2025
     END IF
   END IF
 ! ----------------------------------------------------------------------
+  MY_SAMPLES=PARAMS%NSAMPLES
+! ----------------------------------------------------------------------
   ALLOCATE(MY_FIRST(ISTART:IEND),SOURCE=0_dpi)
   ALLOCATE(MY_LAST(ISTART:IEND),SOURCE=0_dpi)
+  ALLOCATE(PSIJ_FLAT(ISTART:IEND),SOURCE=CMPLX(0._dpf,0._dpf,KIND=dpf))
 ! ----------------------------------------------------------------------
   DO IDX=ISTART,IEND
-    I=1_spi+(IDX-1_spi)/PARAMS%NT
-    J=MOD((IDX-1_spi),PARAMS%NT)+1_spi
+    J=1_spi+(IDX-1_spi)/PARAMS%NS
+    I=MOD((IDX-1_spi),PARAMS%NS)+1_spi
     MY_FIRST(IDX)=PHI%START_IDX(I,J)
     MY_LAST(IDX)=PHI%START_IDX(I,J)+PHI%NUM_ELEMENTS(I,J)-1
   END DO
@@ -79,51 +83,38 @@ PROGRAM NQMCC_ALCF_2025
     PRINT *, "INIT TIME (s)",INIT-FIRST
   END IF
 ! ----------------------------------------------------------------------
-  DO IDX=ISTART,IEND
-    CX=0._dpf
-    DO N=MY_FIRST(IDX),MY_LAST(IDX)
-      CX=CX+YLM_PROD(PHI%YLM_IDX(N))*PHI%PHI_DAT(N)
-    END DO
-    I=1_spi+(IDX-1_spi)/PARAMS%NT
-    J=MOD((IDX-1_spi),PARAMS%NT)+1_spi
-    PSIJ2(I,J)=CX
-  END DO
+  DO S=1,MY_SAMPLES
 ! ----------------------------------------------------------------------
-! should be gather
-  CALL MPI_REDUCE(PSIJ2,PSIJ,MAX_IJ,MPI_COMPLEX16,MPI_SUM,ROOT,COMM,IERROR)
-! ----------------------------------------------------------------------
-! ALL RANKS DO THIS LOOP MANY TIMES : 6*A + 1 * 3 * (A CHOOSE 2)
+! ALL WALKERS DO THIS LOOP MANY TIMES FOR EACH SAMPLE : 6*A + 1 * 3 * (A CHOOSE 2)
 ! TASK 1: 
 !  SPLIT THIS LOOP UP FOR 1 NODE ON CPU USING MPI
-!   => ONE SIDED COMM
 !   => EACH CORE GETS SOME PARTITION OF I,J SPACE
 ! TASK 2: 
 !  OFFLOAD TO GPU
 ! ----------------------------------------------------------------------
-!  DO J=1,PARAMS%NT
-!    DO I=1,PARAMS%NS
-!      CX=0._dpf
-!      FIRST_IDX=PHI%START_IDX(I,J)
-!      LAST_IDX=PHI%START_IDX(I,J)+PHI%NUM_ELEMENTS(I,J)-1
-!      DO N=FIRST_IDX,LAST_IDX
-!        CX=CX+YLM_PROD(PHI%YLM_IDX(N))*PHI%PHI_DAT(N)
-!      END DO
-!      PSIJ(I,J)=CX
-!    END DO
-!  END DO
+    DO IDX=ISTART,IEND
+      CX=0._dpf
+      DO N=MY_FIRST(IDX),MY_LAST(IDX)
+        CX=CX+YLM_PROD(PHI%YLM_IDX(N))*PHI%PHI_DAT(N)
+      END DO
+      PSIJ_FLAT(IDX)=CX
+    END DO
+    CALL MPI_GATHER(PSIJ_FLAT,PARTITION,MPI_COMPLEX16,PSIJ,PARTITION,MPI_COMPLEX16,ROOT,COMM,IERROR)
 ! ----------------------------------------------------------------------
+    IF (RANK.EQ.0) THEN
+      DOT=0._dpf
+      DO J=1,PARAMS%NT
+        DO I=1,PARAMS%NS
+          DOT=DOT+PSIJ(I,J)%RE*PSIJ(I,J)%RE+PSIJ(I,J)%IM*PSIJ(I,J)%IM
+        END DO
+      END DO
+    END IF
+  END DO
   IF (RANK.EQ.ROOT) THEN
     LAST = omp_get_wtime()
-    PRINT *, "PHI TIME (s)",LAST-INIT
-    DOT=0._dpf
-    DO J=1,PARAMS%NT
-      DO I=1,PARAMS%NS
-        DOT=DOT+PSIJ(I,J)%RE*PSIJ(I,J)%RE+PSIJ(I,J)%IM*PSIJ(I,J)%IM/REAL(PARAMS%NPHIM,KIND=dpf)
-      END DO
-    END DO
-    INIT = omp_get_wtime()
-    PRINT *, "DOT TIME (s)",INIT-LAST
-    PRINT *, "DOT ERROR: ",ABS(DOT-26097012.832728475_dpf)
+    PRINT *, "SAMPLE TIME (s)",LAST-INIT
+    PRINT *, "TIME PER SAMPLE (s)",(LAST-INIT)/PARAMS%NSAMPLES
+    IF (PARAMS%NT.EQ.70) PRINT *, "DOT ERROR: ",ABS(DOT-35627044.876464307_dpf)
     PRINT *, "TOTAL TIME (s)",LAST-FIRST
   END IF
 ! ----------------------------------------------------------------------
